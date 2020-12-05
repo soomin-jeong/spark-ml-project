71ef7ee30b21b65da6cd361eaab97217bd1273bc
tomknip <45173142+tomknip@users.noreply.github.com>
2020-12-05 20:16:52 +0100

    Version 1.1 with better use of Pipelines

--- tool/Tool_with_cross_validation.py
+++ tool/Tool_with_cross_validation.py
@@ -1,16 +1,17 @@
 # Import all needed functions
+import time
 from pyspark.sql import SparkSession
 from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
 from pyspark.ml.regression import LinearRegression, DecisionTreeRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import VectorAssembler, OneHotEncoder, StringIndexer
 from pyspark.ml import Pipeline
 from pyspark.ml.tuning import CrossValidator
 from pyspark.sql.types import IntegerType
 
 # SET UP OF ENVIRONMENT
 # Create a Spark Session
-spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.appName('Delay Classifier').master('local[*]').getOrCreate()
 
 # Print the version
 print("Spark version", spark.version)
@@ -31,29 +32,32 @@ print("Data loaded")
 airports = airports.withColumn('CRSDepTime', airports['CRSDepTime'].cast(IntegerType())) \
                     .withColumn('ArrDelay', airports['ArrDelay'].cast(IntegerType())) \
                     .withColumn('DepDelay', airports['DepDelay'].cast(IntegerType())) \
-                    .withColumn('Distance', airports['Distance'].cast(IntegerType()))
-                    #.withColumn('DayOfWeek', airports['DayOfWeek'].cast(IntegerType())) \
-                    #.withColumn('DayOfMonth', airports['DayOfMonth'].cast(IntegerType())) \
+                    .withColumn('Distance', airports['Distance'].cast(IntegerType())) \
+                    .withColumn('DayOfWeek', airports['DayOfWeek'].cast(IntegerType())) \
+                    .withColumn('Month', airports['Month'].cast(IntegerType())) \
                     #.withColumn('DepTime', airports['DepTime'].cast(IntegerType())) \
 
 #airports.printSchema()
 
+# Transform strings to categories
+indexer = StringIndexer(inputCols=['UniqueCarrier'], outputCols=['carrierIndex'])
+
+# One hot encoding
+encoder = OneHotEncoder(inputCols=['DayOfWeek', 'Month', 'carrierIndex'],
+                        outputCols=['DayOfWeekVec', 'MonthVec', 'carrierVec'])
+
 # Filter all the rows where ArrDelay is unknown
 airports = airports.where(airports.ArrDelay.isNotNull())
+# Split data
+flights_train, flights_test = airports.randomSplit([0.9, 0.1], seed=123)
+flights_train.show()
 
 # FEATURE SELECTION
 assembler = VectorAssembler(inputCols=[
-    'DepDelay', 'Distance'
+    'DepDelay', 'DayOfWeekVec', 'MonthVec', 'carrierVec'
 ], outputCol='features')
 
 # MACHINE LEARNING
-airports_prep = assembler.transform(airports)
-airports_final = airports_prep.select(['features',  'ArrDelay'])
-
-# Split data
-flights_train, flights_test = airports_final.randomSplit([0.9, 0.1], seed=123)
-flights_train.show()
-
 # Regressor Choice
 # Maybe an idea to let user choose different algorithms
 lr = LinearRegression(featuresCol= 'features', labelCol='ArrDelay')
@@ -71,10 +75,14 @@ params = params.addGrid(lr.regParam, [0.01]) \
 params = params.build()
 
 # Construct a pipeline
-pipeline = Pipeline(stages=[lr])
+pipeline = Pipeline(stages=[indexer, encoder, assembler, lr])
 
+print("Start Model Fitting")
+start = time.time()
 cv = CrossValidator(estimator=pipeline, estimatorParamMaps=params, evaluator=rmse, numFolds=5)
 cv_model = cv.fit(flights_train)
+print("Time taken to develop model: " + str(time.time()-start) + 's.')
 
-print("RMSE training:" + str(cv_model.summary.rootMeanSquaredError))
-print('RMSE test:' + str(rmse.evaluate(cv_model.transform(flights_test))))
+
+print("RMSE training: " + str(rmse.evaluate(cv_model.bestModel.transform(flights_train))))
+print('RMSE test: ' + str(rmse.evaluate(cv_model.bestModel.transform(flights_test))))
