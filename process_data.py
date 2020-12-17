6e955827e4a17e369673d3e4c2439784cab335c2
soomin-jeong <soominjeongkr@gmail.com>
2020-12-17 10:03:31 +0100

    random forest model

--- process_data.py
+++ process_data.py
@@ -6,34 +6,27 @@ from pyspark.sql.types import IntegerType
 
 spark = SparkSession.builder.appName('Delay Classifier').master('local[*]').getOrCreate()
 
-FORBIDDEN_VARS = ["ArrTime", "ActualElapsedTime", "AirTime", "TaxiIn", "Diverted", "CarrierDelay", "WeatherDelay",
-                  "NASDelay", "SecurityDelay", "LateAircraftDelay"]
+FORBIDDEN_VARS = ["ArrTime", "ActualElapsedTime", "AirTime", "TaxiIn", "Diverted", "CarrierDelay", "WeatherDelay", "NASDelay", "SecurityDelay", "LateAircraftDelay"]
 
 
 class DataProcessor(object):
     def drop_forbidden_variables(self, dataset):
-        return dataset.drop(FORBIDDEN_VARS)
+        return dataset.drop(FORBIDDEN_VARS, axis=1)
 
     def drop_duplicated_data(self, dataset):
-        return dataset.dropDuplicates()
-
-    def convert_datatypes(self, dataset):
-        return dataset.withColumn('CRSDepTime', dataset['CRSDepTime'].cast(IntegerType())) \
-                    .withColumn('ArrDelay', dataset['ArrDelay'].cast(IntegerType())) \
-                    .withColumn('DepDelay', dataset['DepDelay'].cast(IntegerType())) \
-                    .withColumn('Distance', dataset['Distance'].cast(IntegerType())) \
-                    .withColumn('DayOfWeek', dataset['DayOfWeek'].cast(IntegerType())) \
-                    .withColumn('Month', dataset['Month'].cast(IntegerType()))
+        return dataset.drop_duplicates()
 
     def remove_null_arr_delay(self, dataset):
         return dataset.dropna(subset=['ArrDelay'])
 
     def remove_cancelled_flights(self, dataset):
         no_cancelled_flights = dataset[dataset['Cancelled'] == 0]
-        return no_cancelled_flights.pop('Cancelled')
+        no_cancelled_flights.pop('Cancelled')
+        return no_cancelled_flights
 
     def remove_cancelleation_code(self, dataset):
-        return dataset.drop("CancellationCode", axis=1, inplace=True)
+        dataset.pop("CancellationCode")
+        return dataset
 
     def convert_date_fields(self, dataset):
         if 'DepTS' in dataset and 'CSRDepTS' in dataset:
@@ -44,6 +37,7 @@ class DataProcessor(object):
         # Actual departure time
         temp_ts['Hour'] = (dataset["DepTime"] // 100).astype(int).astype(str)
         temp_ts['Minute'] = (dataset["DepTime"] % 100).astype(int).astype(str)
+
         temp_ts['Time'] = temp_ts['Year'] + '-' + temp_ts['Month'] + '-' + temp_ts['DayofMonth'] + ' ' \
                           + temp_ts['Hour'] + ':' + temp_ts['Minute']
         temp_ts['DepTS'] = pd.to_datetime(temp_ts['Time'], format='%Y-%m-%d %H:%M', errors='coerce')
@@ -55,18 +49,22 @@ class DataProcessor(object):
                              temp_ts['CSRDepHour'] + ':' + temp_ts['CSRDepMinute']
         temp_ts['CSRDepTS'] = pd.to_datetime(temp_ts['CSRTime'], format='%Y-%m-%d %H:%M', errors='coerce')
 
-        dataset['DepTS'] = temp_ts['DepTS']
-        dataset['CSRDepTS'] = temp_ts['CSRDepTS']
+        # dataset['DepTS'] = temp_ts['DepTS']
+        dataset['DepHour'] = temp_ts['Hour']
+        dataset['DepMinute'] = temp_ts['Minute']
+
+        # dataset['CSRDepTS'] = temp_ts['CSRDepTS']
+        dataset['CSRDepHour'] = temp_ts['CSRDepHour']
+        dataset['CSRDepMinute'] = temp_ts['CSRDepMinute']
         return dataset
 
     def run_all_data_processing(self, dataset):
         dataset = self.drop_duplicated_data(dataset)
-        dataset = self.convert_datatypes(dataset)
         dataset = self.drop_forbidden_variables(dataset)
         dataset = self.remove_null_arr_delay(dataset)
         dataset = self.remove_cancelled_flights(dataset)
         dataset = self.remove_cancelleation_code(dataset)
-        dataset = self.convert_datatypes(dataset)
+        dataset = self.convert_date_fields(dataset)
         return dataset
 
 data_processor = DataProcessor()
--- train_data.py
+++ train_data.py
@@ -1,16 +1,83 @@
-from pyspark.ml import Pipeline
-from pyspark.ml.classification import DecisionTreeClassifier
-from pyspark.ml.feature import StringIndexer, VectorIndexer
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+import time
+
 from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType
+
+from pyspark.ml import Pipeline
+from pyspark.ml.regression import RandomForestRegressor
+from pyspark.ml.feature import VectorIndexer, StringIndexer
+from pyspark.ml.evaluation import RegressionEvaluator
 
 from process_data import data_processor as dp
+from load_data import data_loader as dl
 
 
 class DataTrainer(object):
 
-    def ramdom_forest(self, dataset):
-        return
+    def random_forest(self):
+        # SET UP OF ENVIRONMENT
+        # Create a Spark Session
+        spark = SparkSession.builder.config("spark.driver.memory", "15g").appName('Delay Classifier').master('local[*]').getOrCreate()
+
+        # Load the data
+        raw_dataset = dl.load_dataset()
+        pd_data = dp.run_all_data_processing(raw_dataset)
+        pd_data.pop('UniqueCarrier')
+        pd_data.pop('DepTime')
+        pd_data.pop('CRSDepTime')
+        pd_data.pop('CRSArrTime')
+
+        pd_data.pop('Origin')
+        pd_data.pop('Dest')
+
+
+        data = spark.createDataFrame(pd_data)
+
+        data = data.withColumn('ArrDelay', data['ArrDelay'].cast(IntegerType())) \
+            .withColumn('DepDelay', data['DepDelay'].cast(IntegerType())) \
+            .withColumn('Distance', data['Distance'].cast(IntegerType())) \
+            .withColumn('DayOfWeek', data['DayOfWeek'].cast(IntegerType())) \
+            .withColumn('Month', data['Month'].cast(IntegerType())) \
+            .withColumn('DepHour', data['DepHour'].cast(IntegerType())) \
+            .withColumn('DepMinute', data['DepMinute'].cast(IntegerType())) \
+            .withColumn('CSRDepHour', data['CSRDepHour'].cast(IntegerType())) \
+            .withColumn('CSRDepMinute', data['CSRDepMinute'].cast(IntegerType()))
+
+        stringIndexer = \
+            StringIndexer(inputCols=['DayOfWeek', 'Month'],
+                          outputCols=['DayOfWeek_idx', 'Month_idx'])
+
+        # Split the data into training and test sets (30% held out for testing)
+        (trainingData, testData) = data.randomSplit([0.7, 0.3])
+
+        # Train a RandomForest model..
+        rf = RandomForestRegressor()
+
+        # Chain indexer and forest in a Pipeline
+        pipeline = Pipeline(stages=[stringIndexer, rf])
+
+        print("64")
+
+        # Train model.  This also runs the indexer.
+        model = pipeline.fit(trainingData)
+
+        # Make predictions.
+        predictions = model.transform(testData)
+
+        # Select example rows to display.
+        predictions.select("prediction", "ArrDelay").show(5)
+
+        # Select (prediction, true label) and compute test error
+        evaluator = RegressionEvaluator(
+            labelCol="ArrDelay", predictionCol="prediction", metricName="rmse")
+        rmse = evaluator.evaluate(predictions)
+        print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
+
+        rfModel = model.stages[1]
+        print(rfModel)  # summary only
+
+        spark.stop()
 
 
 data_trainer = DataTrainer()
+data_trainer.random_forest()
