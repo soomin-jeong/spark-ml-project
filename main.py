2db0faac3462a698f485ada98e169c403c207820
soomin-jeong <soominjeongkr@gmail.com>
2020-12-20 10:55:56 +0100

    added memory choice

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ RANDOM_FOREST = 3
 
 ALGORITHM_OPTIONS = [LINEAR_REGRESSION, DECISION_TREE, RANDOM_FOREST]
 
+DEFAULT_SPARK_MEMORY_SIZE = "1g"
 
 class MachineLearningRunner(object):
-
-    def __init__(self, algorithm):
-        self.spark = SparkSession.builder.appName('Delay Classifier').config("spark.driver.memory", "15g").master('local[*]').getOrCreate()
+    def __init__(self, algorithm, memory_size=DEFAULT_SPARK_MEMORY_SIZE):
+        self.spark = SparkSession.builder.appName('Delay Classifier').config("spark.driver.memory", memory_size).master('local[*]').getOrCreate()
         self.dl = DataLoader(self.spark)
         self.dp = DataProcessor(self.spark)
         self.dt = DataTrainer(self.spark, self.dp)
@@ -79,8 +79,20 @@ class Launcher(object):
     def check_algorithm(self, algorithm):
         # TODO: Are there any exceptions to take care of?
 
-        if algorithm not in ALGORITHM_OPTIONS:
+        if not algorithm.isnumeric():
+            return False, "algorithm choice should be a number"
+
+        if int(algorithm) not in ALGORITHM_OPTIONS:
             return False, "algorithm should be {}, {} or {}".format(*ALGORITHM_OPTIONS)
+        return True, None
+
+    def check_memorysize(self, memory_size):
+        if not memory_size.isnumeric():
+            return False, "memory size should be a number"
+
+        if memory_size < 1:
+            return False, "Recommended memory size is over 1 GB"
+
         return True, None
 
 
@@ -95,29 +107,43 @@ if __name__ == '__main__':
     correct_syntax = False
     data_filepath = os.path.join(os.getcwd(), 'input_dataset', 'dataset.csv')
     algorithm = LINEAR_REGRESSION
+    memory_size = DEFAULT_SPARK_MEMORY_SIZE
 
     while not correct_syntax:
+        # filepath for raw dataset
         data_filepath_input = input("Enter the filepath of a CSV dataset (by default, input_dataset/dataset.csv): ")
+
+        if data_filepath_input:
+            right_file_path, f_error_msg = launcher.check_filepath(data_filepath_input)
+            if not right_file_path:
+                print(f_error_msg + ", Enter again...")
+                continue
+            data_filepath = data_filepath_input
+
+        # algorithm for machine learning
         algorithm_input = input("Enter the machine learning algorithm (1: Linear Regression, 2:Decision Tree, 3:Random Forest, "
                           "by default Linear Regression): ")
 
-        if data_filepath_input:
-            data_filepath = data_filepath_input
         if algorithm_input:
-            algorithm = int(algorithm_input)
+            algorithm_check, a_error_msg = launcher.check_algorithm(algorithm_input)
+            if not algorithm_check:
+                print(a_error_msg + ", Enter again...")
+                continue
+            algorithm = algorithm_input
 
-        right_file_path, f_error_msg = launcher.check_filepath(data_filepath)
-        if not right_file_path:
-            print(f_error_msg + ", Enter again...")
-            continue
+        # setting memory size for spark
+        memory_size_input = input("Enter the memory size for spark in GB (by default, 1): ")
 
-        algorithm_check, a_error_msg = launcher.check_algorithm(algorithm)
-        if not algorithm_check:
-            print(a_error_msg + ", Enter again...")
-            continue
+        if memory_size_input:
+            memory_size_check, m_error_msg = launcher.check_memorysize(memory_size_input)
+            if not memory_size_check:
+                print(m_error_msg + ", Enter again...")
+                continue
+            memory_size = memory_size_input
+
         correct_syntax = True
 
-    ml_runner = MachineLearningRunner(algorithm)
+    ml_runner = MachineLearningRunner(algorithm, memory_size)
     ml_runner.run(data_filepath)
 
 
